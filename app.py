30103221e4965a2d083939d5987c524502043ad8
Sandhya <sandhaybacchewar50@gmail.com>
2026-04-22 12:48:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    return render_template('success.html', name=name, email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
